cf1e9666d9ccd218db5ab54b72c2e1737e21288f
KN <komrad-shiryaev@yandex.ru>
2020-10-27 00:01:32 +0500

    В test_main_page добавил новый метод для проверки страницы логина

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,14 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 from pages.locators import MainPageLocators
+from pages.login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+
 
     def should_be_to_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
